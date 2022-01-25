2153ce5a745017e4f4d970372b342abfcafd0b28
RISHAVJ0212 <97506486+RISHAVJ0212@users.noreply.github.com>
2022-01-25 11:10:32 +0530

    Add files via upload

---
+++ ASSIGNMENT_OUTPUT.py
@@ -0,0 +1,98 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: F:\PYTHON\S_PROGRMAS\ASSIGNMENT_2\ASSIGNMENT_CODE.PY ========
+
+ -------------------------------------------------- 
+                         1                          
+ -------------------------------------------------- 
+ 
+
+Given String is  : 
+ Python is a case sensitive language
+
+------- a ------
+
+Length of Given String is	:	 35
+
+------- b ------
+
+Reversed order of Given Sting is   :
+ egaugnal evitisnes esac a si nohtyP
+
+------- c ------
+
+a case sensitive
+
+------- d ------
+
+After Replacing, Sting is  :
+ Python is object oriented language
+
+------- e ------
+
+the position of a is  10
+
+------- f ------
+
+After removing the white, String is :
+ Pythonisacasesensitivelanguage
+
+ -------------------------------------------------- 
+                         2                          
+ -------------------------------------------------- 
+ 
+
+Enter your Name	:	Rishav Jindal
+Enter your SID	:	21104095
+Enter your Department Name	:	Electrical
+Enter your CGPA	:	9.9
+Hey,Rishav Jindal here! 
+ My SID is 21104095 
+ I am from Electrical department and my CGPA is 9.9
+
+
+ -------------------------------------------------- 
+                         3                          
+ -------------------------------------------------- 
+ 
+
+a= 56 b= 10
+'a&b' is 	:	 8
+'a|b' is 	:	 58
+'a^b' is 	:	 50
+'a' and 'b' after shifting left with 2 bits are 	:	 224 and 40
+'a' and 'b' after shifting right with 2 and 4 bits respectively are 	:	 14 and 0
+
+
+ -------------------------------------------------- 
+                         4                          
+ -------------------------------------------------- 
+ 
+
+To find the largest number out of 3 input numbers
+Enter the First Number	:	545.4
+Enter the second number	:	545.9999999
+Enter the third number	:	454.26
+The Largest Number is	:	 545.9999999
+
+
+ -------------------------------------------------- 
+                         5                          
+ -------------------------------------------------- 
+ 
+
+To check if word'name'is in data entered by you
+Enter the sentence in which you wnat to check 'name' word	:	My n@me is Rishav Jindal
+No
+
+ -------------------------------------------------- 
+                         6                          
+ -------------------------------------------------- 
+ 
+
+To check if Tra=iangle is possible or not
+Enter the First Number	:	6
+Enter the second number	:	8
+Enter the third number	:	10
+Yes
